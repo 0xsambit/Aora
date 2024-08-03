28436f3a9f39088854745d51b56e6537d5796cbd
0xsambit <sambitsingha777@gmail.com>
2024-08-04 01:06:30 +0530

    Statusbar added

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -2,6 +2,7 @@ import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { icons } from "../../constants";
+import { StatusBar } from "expo-status-bar";
 
 const TabIcon = ({ color, icon, name, focused }) => {
 	return (
@@ -23,79 +24,82 @@ const TabIcon = ({ color, icon, name, focused }) => {
 
 const TabsLayout = () => {
 	return (
-		<Tabs
-			screenOptions={{
-				tabBarShowLabel: false,
-				tabBarActiveTintColor: "#FFA001",
-				tabBarInactiveTintColor: "#CDCDE0",
-				tabBarStyle: {
-					backgroundColor: "#161622",
-					borderTopWidth: 1,
-					borderTopColor: "#232533",
-					height: 80,
-				},
-			}}>
-			<Tabs.Screen
-				name='home'
-				options={{
-					title: "Home",
-					headerShown: false,
-					tabBarIcon: ({ color, focused }) => (
-						<TabIcon
-							icon={icons.home}
-							name='Home'
-							color={color}
-							focused={focused}
-						/>
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name='bookmark'
-				options={{
-					title: "Bookmark",
-					headerShown: false,
-					tabBarIcon: ({ color, focused }) => (
-						<TabIcon
-							icon={icons.bookmark}
-							name='Bookmark'
-							color={color}
-							focused={focused}
-						/>
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name='create'
-				options={{
-					title: "Create",
-					headerShown: false,
-					tabBarIcon: ({ color, focused }) => (
-						<TabIcon
-							icon={icons.plus}
-							name='Create'
-							color={color}
-							focused={focused}
-						/>
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name='profile'
-				options={{
-					title: "Profile",
-					headerShown: false,
-					tabBarIcon: ({ color, focused }) => (
-						<TabIcon
-							icon={icons.profile}
-							name='Profile'
-							color={color}
-							focused={focused}
-						/>
-					),
-				}}
-			/>
-		</Tabs>
+		<>
+			<Tabs
+				screenOptions={{
+					tabBarShowLabel: false,
+					tabBarActiveTintColor: "#FFA001",
+					tabBarInactiveTintColor: "#CDCDE0",
+					tabBarStyle: {
+						backgroundColor: "#161622",
+						borderTopWidth: 1,
+						borderTopColor: "#232533",
+						height: 80,
+					},
+				}}>
+				<Tabs.Screen
+					name='home'
+					options={{
+						title: "Home",
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<TabIcon
+								icon={icons.home}
+								name='Home'
+								color={color}
+								focused={focused}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name='bookmark'
+					options={{
+						title: "Bookmark",
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<TabIcon
+								icon={icons.bookmark}
+								name='Bookmark'
+								color={color}
+								focused={focused}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name='create'
+					options={{
+						title: "Create",
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<TabIcon
+								icon={icons.plus}
+								name='Create'
+								color={color}
+								focused={focused}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name='profile'
+					options={{
+						title: "Profile",
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<TabIcon
+								icon={icons.profile}
+								name='Profile'
+								color={color}
+								focused={focused}
+							/>
+						),
+					}}
+				/>
+			</Tabs>
+			<StatusBar backgroundColor='#161622' style='light' />
+		</>
 	);
 };
 
